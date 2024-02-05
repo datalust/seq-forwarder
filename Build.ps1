7fe86a4d035f200903428b000f1e07c09ab403d9
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2024-02-05 10:25:06 +1000

    Make sure the CI build fails when steps fail

--- Build.ps1
+++ Build.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = 'Stop'
 
-$framework = 'netcoreapp3.1'
+$framework = 'net8.0'
 
 function Clean-Output
 {
@@ -18,7 +18,7 @@ function Execute-Tests
 {
     Write-Output "Testing native platform version"
     & dotnet test ./test/Seq.Forwarder.Tests/Seq.Forwarder.Tests.csproj -c Release /p:Configuration=Release /p:Platform=x64 /p:VersionPrefix=$version
-    if($LASTEXITCODE -ne 0) { exit 3 }
+    if($LASTEXITCODE -ne 0) { throw "failed" }
 }
 
 function Create-ArtifactDir
@@ -29,28 +29,28 @@ function Create-ArtifactDir
 
 function Publish-Archives($version)
 {
-	$rids = @("linux-x64", "osx-x64", "win-x64")
+	$rids = @("linux-x64", "osx-x64", "linux-arm64", "osx-arm64", "win-x64")
 	foreach ($rid in $rids) {
         Write-Output "Publishing archive for $rid"
 		
 		& dotnet publish src/Seq.Forwarder/Seq.Forwarder.csproj -c Release -f $framework -r $rid /p:VersionPrefix=$version /p:SeqForwarderRid=$rid
-		if($LASTEXITCODE -ne 0) { exit 4 }
+        if($LASTEXITCODE -ne 0) { throw "failed" }
 
 		# Make sure the archive contains a reasonable root filename
 		mv ./src/Seq.Forwarder/bin/Release/$framework/$rid/publish/ ./src/Seq.Forwarder/bin/Release/$framework/$rid/seqfwd-$version-$rid/
 
 		if ($rid.StartsWith("win-")) {
 			& ./build/7-zip/7za.exe a -tzip ./artifacts/seqfwd-$version-$rid.zip ./src/Seq.Forwarder/bin/Release/$framework/$rid/seqfwd-$version-$rid/
-			if($LASTEXITCODE -ne 0) { exit 5 }
+            if($LASTEXITCODE -ne 0) { throw "failed" }
 		} else {
 			& ./build/7-zip/7za.exe a -ttar seqfwd-$version-$rid.tar ./src/Seq.Forwarder/bin/Release/$framework/$rid/seqfwd-$version-$rid/
-			if($LASTEXITCODE -ne 0) { exit 5 }
+            if($LASTEXITCODE -ne 0) { throw "failed" }
 
 			# Back to the original directory name
 			mv ./src/Seq.Forwarder/bin/Release/$framework/$rid/seqfwd-$version-$rid/ ./src/Seq.Forwarder/bin/Release/$framework/$rid/publish/
 			
 			& ./build/7-zip/7za.exe a -tgzip ./artifacts/seqfwd-$version-$rid.tar.gz seqfwd-$version-$rid.tar
-			if($LASTEXITCODE -ne 0) { exit 6 }
+            if($LASTEXITCODE -ne 0) { throw "failed" }
 
 			rm seqfwd-$version-$rid.tar
 		}
